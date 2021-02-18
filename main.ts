81841dd0ae83687aea6483c3940d5629316038b8
Rob <rlmartain@hotmail.com>
2021-02-18 20:50:58 +1100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    DualChannelFullHMovement.Move(movementController, DualChannelFullHMovement.MoveDirection.Forward, 512)
 })
+input.onButtonPressed(Button.B, function () {
+    DualChannelFullHMovement.Move(movementController, DualChannelFullHMovement.MoveDirection.Backward, 512)
+})
+let movementController: DualChannelFullHMovement.MotorController = null
+movementController = DualChannelFullHMovement.SetupMotorController(
+AnalogPin.P1,
+AnalogPin.P9,
+AnalogPin.P10,
+AnalogPin.P15
+)
